33ee8c275cf61dc7bd42124ac97cbdba2691e893
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-09-06 21:50:16 -0400

    init

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const { View, Text } = require("react-native")
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home Componenet</Text>
+        </View>
+    )
+}
+
+export default Home
